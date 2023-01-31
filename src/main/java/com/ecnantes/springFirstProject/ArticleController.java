b0678570f87e9b8145899338c92d72c8cc0e7c6b
Lemasson Steven <lemasson.steven@gmail.com>
2023-01-31 10:11:03 +0100

    tp5 done

--- src/main/java/com/ecnantes/springFirstProject/ArticleController.java
+++ src/main/java/com/ecnantes/springFirstProject/ArticleController.java
@@ -4,7 +4,10 @@ import com.ecnantes.springFirstProject.Model.ArticleModel;
 import com.ecnantes.springFirstProject.Model.Statut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/article")
@@ -13,22 +16,22 @@ public class ArticleController {
     private Logger logger = LoggerFactory.getLogger(ArticleController.class);
 
     @GetMapping
-    public ArticleModel getArticle() {
-        ArticleModel article = new ArticleModel();
-        article.setAuteur("Lemasson Steven");
-        article.setTitle("Toute ma vie!");
-        article.setContenu("Hello World!");
-        article.setStatut(Statut.PUBLIE);
-        return article;
+    public ResponseEntity<ArticleModel> getArticle(HttpSession session) {
+        ArticleModel article = (ArticleModel) session.getAttribute("article");
+        if(article == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(article);
     }
 
     @PostMapping
-    public ArticleModel creerArticle(@RequestBody ArticleModel article) {
+    public ArticleModel creerArticle(@RequestBody ArticleModel article, HttpSession session) {
         if(article.getAuteur() == null) {
             logger.error("L'article n'a pas d'auteur");
             return null;
         } else {
             logger.info("Titre {} Auteur {}", article.getTitle(), article.getAuteur());
+            session.setAttribute("article", article);
             return  article;
         }
     }
